5eb4c98a29911129ae298807be6af9886380f055
Amit Shrivastava <amit.shrivastava@talentica.com>
2015-09-30 21:23:36 +0530

    Changed sample data

--- customer_management_artifact/src/main/resources/sample_data.sql
+++ customer_management_artifact/src/main/resources/sample_data.sql
@@ -1,6 +1,6 @@
-INSERT INTO customer (email, first_name,last_name)  VALUES ('amit.shrivastava@talentica.com','Amit','Shrivastava');
-INSERT INTO customer (email, first_name,last_name)  VALUES ('ashish2402@gmail.com','Ashish','Shrivastava');
-INSERT INTO customer (email, first_name,last_name)  VALUES ('ajeetj@fidelis-inc.com','Ajeet','Jain');
+INSERT INTO customer (email, first_name,last_name)  VALUES ('amit.shrivastava@rediff.com','Amit','Shrivastava');
+INSERT INTO customer (email, first_name,last_name)  VALUES ('ashish2402@rediff.com','Ashish','Shrivastava');
+INSERT INTO customer (email, first_name,last_name)  VALUES ('amish@hotmail.com','Amish','Jain');
 
 INSERT INTO address (city, locality, pin_code, state, customer_id) VALUES ('Pune', 'Baner', '411045', 'Maharastra', 1);
 INSERT INTO address (city, locality, pin_code, state, customer_id) VALUES ('Pune', 'Baner', '411045', 'Maharastra', 2);
